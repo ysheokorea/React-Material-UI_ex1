70ce61c87333bb5aa4feed0149727d31648a3e8f
ys <ys.heo.korea@gmail.com>
2022-08-11 08:43:47 +0900

    initial

---
+++ src/components/Feed.jsx
@@ -0,0 +1,8 @@
+import { Box } from '@mui/material';
+import React from 'react';
+
+export default function Feed() {
+    return (
+        <Box bgcolor={'pink'} flex={4} p={2}>Feed</Box>
+    )
+}
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,8 @@
+import { Box } from '@mui/material';
+import React from 'react';
+
+export default function Sidebar() {
+    return (
+        <Box backgroundColor='skyblue' flex={1} p={2} sx={{display:{xs:"none", sm:"block"}}}>Sidebar</Box>
+    )
+}
